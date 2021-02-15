5d3c7b0cd1feb5dc8436e06c5f62067dc061c90f
sveta <svetlana77ekb@yandex.ru>
2021-02-15 17:11:14 +0500

    Lesson 4. Auth app and user model

---
+++ authnapp/admin.py
@@ -0,0 +1,20 @@
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import ShopUser
+
+
+class ShopUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "age"),
+            },
+        ),
+    )
+
+
+admin.site.register(ShopUser, ShopUserAdmin)
